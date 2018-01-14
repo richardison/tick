50552e2b4d8eda09b8444d04fce17771844626f0
isonr <rison@foko.co>
2018-01-14 09:24:16 -0500

    Util for getting number of weeks for month

--- test/getters/weeksof/month.test.js
+++ test/getters/weeksof/month.test.js
@@ -2,8 +2,7 @@ const test = require('ava')
 const $ = require('../../utils/constants')
 const Tick = require('../../../')
 
-test('should get weekOfMonth from date', t => {
-  t.is(Tick.getWeekOfMonth($.TODAY), 2, 'today is the second week of the month')
-  t.is(Tick.getWeekOfMonth($.END_OF_JAN_2017), 5, 'end of jan is the fifth week of the month')
-  t.is(Tick.getWeekOfMonth($.FEB_1_1980), 1, 'feb 1 is the first week of the month')
+test('should get weeksOfMonth from date', t => {
+  t.is(Tick.getWeeksOfMonth($.TODAY), 5, 'January 2018 has 5 weeks')
+  t.is(Tick.getWeeksOfMonth($.FEB_1_1933), 4, 'Feb 1933 has 4 weeks')
 })
